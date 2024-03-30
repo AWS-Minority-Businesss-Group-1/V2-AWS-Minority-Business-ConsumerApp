6eb97ebf37b315e5d566baa5cdf5627bd1b3fe40
ChristianASC5 <41081554+ChristianASC5@users.noreply.github.com>
2024-03-30 13:26:16 -0400

    Added accounttype enum

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,9 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "v2awsminoritybusines": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
